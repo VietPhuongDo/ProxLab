410dd6fc8013789e69712343db6b41e92b498416
VietPhuongDo <vietphuongdo511@gmail.com>
2023-12-18 09:19:14 +0700

    update MusicRemote

--- MusicRemote/MusicRemote/ViewController.swift
+++ MusicRemote/MusicRemote/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  MusicRemote
-//
-//  Created by PhuongDo on 14/12/2023.
-//
-
-
 import UIKit
 import AVFoundation
 import Network
@@ -17,116 +9,76 @@ class ViewController: UIViewController {
     @IBOutlet weak var loopButton: UIButton!
     @IBOutlet weak var speedButton: UIButton!
     @IBOutlet weak var timeSlider: UISlider!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
-    private var player: AVPlayer?
+    private var audioPlayer: AVPlayer?
     private var playerItem: AVPlayerItem?
-    private var timeObserver: Any?
     private var updateTimer: Timer?
+    private var isLooping: Bool = false
+    private var speeds: [Float] = [0.5, 0.75, 1.0, 1.25, 1.5, 2.0]
+    private var selectedSpeed: Float = 1.0
     private var pathMonitor: NWPathMonitor?
+    private var isNetworkConnected = true
 
-    var isLooping: Bool = false
-    var speeds: [Float] = [0.5, 0.75, 1.0, 1.25, 1.5, 2.0]
-    var selectedSpeed: Float = 1.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupNetworkMonitoring()
-
-        if let audioURL = URL(string: "http://palestine.procyoni.com/public/file/fast/dbafec6d-3d73-4928-b0a9-4a7cc42d0187.mp3") {
-            playerItem = AVPlayerItem(url: audioURL)
-            player = AVPlayer(playerItem: playerItem)
-
-            if player != nil {
-                let duration = CMTimeGetSeconds(playerItem!.asset.duration)
-                timeSlider.maximumValue = Float(duration)
-                addTimeObserver()
-            }
-
-            print("Audio player initialized successfully.")
-        } else {
-            print("Audio URL is invalid.")
+        guard let audioURL = URL(string: "http://palestine.procyoni.com/public/file/fast/dbafec6d-3d73-4928-b0a9-4a7cc42d0187.mp3") else {
+            fatalError("Invalid audio URL")
         }
+
+        playerItem = AVPlayerItem(url: audioURL)
+        audioPlayer = AVPlayer(playerItem: playerItem)
+
+        audioPlayer?.pause()
 
         timeSlider.minimumValue = 0
+        if let duration = playerItem?.asset.duration.seconds, !duration.isNaN {
+            timeSlider.maximumValue = Float(duration)
+        } else {
+            print("Invalid duration")
+            timeSlider.maximumValue = 0
+        }
+
         timeSlider.value = 0
         timeSlider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
-        startUpdateTimer()
-    }
 
-    func setupNetworkMonitoring() {
-        pathMonitor = NWPathMonitor()
-        pathMonitor?.pathUpdateHandler = { [weak self] path in
-            DispatchQueue.main.async {
-                if path.status == .satisfied {
-                    self?.showPlayButton()
-                } else {
-                    self?.hidePlayButton()
-                    self?.showNetworkAlert()
-                }
-            }
+        setupNetworkMonitoring()
+
+        let endTime = CMTime(seconds: playerItem?.asset.duration.seconds ?? 0, preferredTimescale: 1)
+        audioPlayer?.addBoundaryTimeObserver(forTimes: [NSValue(time: endTime)], queue: nil) { [weak self] in
+            self?.handlePlaybackEnd()
         }
-        let queue = DispatchQueue(label: "NetworkMonitorQueue")
-        pathMonitor?.start(queue: queue)
-    }
-
-    func showPlayButton() {
-        playPauseButton.isHidden = false
-        activityIndicator.stopAnimating()
-    }
-
-    func hidePlayButton() {
-        playPauseButton.isHidden = true
-        activityIndicator.startAnimating()
-    }
-
-    func showNetworkAlert() {
-        let alertController = UIAlertController(title: "No Network Connection", message: "Please check your internet connection.", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: { [weak self] _ in
-            self?.showPlayButton()
-        })
-        alertController.addAction(okAction)
-        present(alertController, animated: true, completion: nil)
     }
 
     @IBAction func playPauseButtonTapped(_ sender: UIButton) {
-        if let player = player {
-            if player.rate == 0 {
-                if let pathMonitor = self.pathMonitor, pathMonitor.currentPath.status == .satisfied {
-                    player.play()
-                    updatePlayPauseButton(imageName: "pause.fill")
-                } else {
-                    showNetworkAlert()
-                }
-            } else {
-                player.pause()
-                updatePlayPauseButton(imageName: "play.fill")
-            }
+        // Check network connection before playing
+        if let currentPath = pathMonitor?.currentPath {
+            handleNetworkUpdate(currentPath)
         }
-    }
 
-    @objc func handlePlayerItemDidPlayToEndTimeNotification(_ notification: Notification) {
-        if let player = player {
-            if isLooping {
-                player.seek(to: .zero)
+        if let player = audioPlayer {
+            if player.rate == 0 {
                 player.play()
+                updatePlayPauseButton(imageName: "pause.fill")
+                startUpdateTimer()
             } else {
                 player.pause()
-                player.seek(to: .zero)
                 updatePlayPauseButton(imageName: "play.fill")
-                updateSlider()
                 stopUpdateTimer()
             }
         }
+        
     }
 
     @IBAction func stopButtonTapped(_ sender: UIButton) {
-        if let player = player {
+        if let player = audioPlayer {
             player.pause()
-            player.seek(to: .zero)
+            player.seek(to: CMTime.zero)
             updatePlayPauseButton(imageName: "play.fill")
-            speedButton.setTitle("1.0x", for: .normal)
+            updateSlider()
+            speedButton.titleLabel?.text = "1.0x"
+            timeSlider.value = 0
         }
     }
 
@@ -134,10 +86,7 @@ class ViewController: UIViewController {
         isLooping.toggle()
         let buttonText = isLooping ? "Looping" : "Normal"
         loopButton.setTitle(buttonText, for: .normal)
-
-        if let player = player {
-            player.actionAtItemEnd = isLooping ? .none : .pause
-        }
+        audioPlayer?.actionAtItemEnd = isLooping ? .none : .pause
     }
 
     @IBAction func speedButtonTapped(_ sender: UIButton) {
@@ -150,7 +99,7 @@ class ViewController: UIViewController {
         for speed in speeds {
             let action = UIAlertAction(title: "\(speed)x", style: .default) { [weak self] _ in
                 self?.selectedSpeed = speed
-                self?.player?.rate = speed
+                self?.audioPlayer?.rate = speed
                 self?.updateSpeedButton()
             }
             alertController.addAction(action)
@@ -163,39 +112,41 @@ class ViewController: UIViewController {
     }
 
     @objc func sliderValueChanged(_ sender: UISlider) {
-        if let player = player {
-            let seconds = Double(sender.value)
-            let targetTime = CMTimeMakeWithSeconds(seconds, preferredTimescale: player.currentTime().timescale)
+        if let player = audioPlayer {
+            let targetTime = CMTime(seconds: Double(sender.value), preferredTimescale: 1)
             player.seek(to: targetTime)
-        }
-    }
-
-    func addTimeObserver() {
-        if let player = player {
-            let timeScale = playerItem?.asset.duration.timescale ?? CMTimeScale(NSEC_PER_SEC)
-            let time = CMTime(seconds: 1, preferredTimescale: timeScale)
-            timeObserver = player.addPeriodicTimeObserver(forInterval: time, queue: DispatchQueue.main) { [weak self] time in
-                self?.updateSlider()
-            }
+            updateSlider()
         }
     }
 
     @objc func updateSlider() {
-        if let player = player {
-            let currentTime = CMTimeGetSeconds(player.currentTime())
-            timeSlider.value = Float(currentTime)
+        guard let player = audioPlayer else { return }
+        timeSlider.value = Float(player.currentTime().seconds)
+    }
+
+
+
+    @objc func handlePlaybackEnd() {
+        if isLooping, let player = audioPlayer {
+            let currentRate = player.rate
+            player.seek(to: CMTime.zero)
+            player.play()
+            player.rate = currentRate
+        } else {
+            updatePlayPauseButton(imageName: "play.fill")
+            updateSlider()
+            stopUpdateTimer()
+            hidePlayButton()
         }
     }
+
+    func hidePlayButton() {
+        playPauseButton.isHidden = true
+    }
+
 
     func startUpdateTimer() {
-        if updateTimer == nil {
-            updateTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateSlider), userInfo: nil, repeats: true)
-        }
-    }
-
-    func stopUpdateTimer() {
-        updateTimer?.invalidate()
-        updateTimer = nil
+        updateTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateSlider), userInfo: nil, repeats: true)
     }
 
     func updatePlayPauseButton(imageName: String) {
@@ -206,8 +157,68 @@ class ViewController: UIViewController {
         speedButton.setTitle("\(selectedSpeed)x", for: .normal)
     }
 
+    func stopUpdateTimer() {
+        updateTimer?.invalidate()
+        updateTimer = nil
+    }
+
+    func setupNetworkMonitoring() {
+        pathMonitor = NWPathMonitor()
+        let queue = DispatchQueue.global(qos: .background)
+        pathMonitor?.start(queue: queue)
+        pathMonitor?.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.handleNetworkUpdate(path)
+            }
+        }
+    }
+
+    func handleNetworkUpdate(_ path: NWPath) {
+            let isConnected = path.status == .satisfied
+
+            if isConnected != isNetworkConnected {
+                isNetworkConnected = isConnected
+                if isConnected {
+                    print("Connected to the network.")
+                    hideNoNetworkAlert()
+                    enableMusicInteraction()
+                } else {
+                    print("Not connected to the network.")
+                    showNoNetworkAlert()
+                    disableMusicInteraction()
+                }
+            }
+        }
+    func showNoNetworkAlert() {
+        let alertController = UIAlertController(title: "No Network Connection", message: "Please check your internet connection and try again.", preferredStyle: .alert)
+
+        let okAction = UIAlertAction(title: "OK", style: .default) { [weak self] _ in
+            self?.handleNoNetworkConnection()
+        }
+        alertController.addAction(okAction)
+
+        present(alertController, animated: true, completion: nil)
+    }
+
+    func hideNoNetworkAlert() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    func disableMusicInteraction() {
+        playPauseButton.isHidden = true
+        audioPlayer?.pause()
+    }
+
+    func enableMusicInteraction() {
+        playPauseButton.isHidden = false
+        playPauseButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+    }
+
+    func handleNoNetworkConnection() {
+    
+    }
+
     deinit {
-        stopUpdateTimer()
-        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+        NotificationCenter.default.removeObserver(self)
     }
 }
